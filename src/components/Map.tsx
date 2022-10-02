9b46d7f919cbe80d917ab603f4acf46df63fcca9
kobakazu0429 <kobakazu10@gmail.com>
2022-10-02 12:55:42 +0900

    add maplibre-gl

---
+++ src/components/Map.tsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useRef } from "react";
+import maplibregl from "maplibre-gl";
+import "maplibre-gl/dist/maplibre-gl.css";
+
+export const Map = () => {
+  const mapContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    if (!mapContainerRef.current) return;
+
+    const map = new maplibregl.Map({
+      container: mapContainerRef.current,
+      style: "https://demotiles.maplibre.org/style.json",
+      center: [135.0066832, 37.9619195],
+      zoom: 4,
+    });
+
+    return () => {
+      map.remove();
+    };
+  }, [mapContainerRef]);
+
+  return (
+    <div
+      ref={mapContainerRef}
+      style={{
+        width: "100%",
+        height: "100%",
+      }}
+    />
+  );
+};
